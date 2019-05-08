f76be3fa26e29e2115ceb636607b0943ad90e39c
Paellis12 <patrick.ellis84@yahoo.com>
2019-05-08 19:25:17 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import cards from "./cards.json";
+import cards from "./components/cards.json";
 import "./App.css";
 
 class App extends Component {
